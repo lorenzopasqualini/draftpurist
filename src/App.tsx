afd5170441b3fed22e0f7e0eb5ff4b8057a7810e
Lorenzo Pasqualini <lorenzopasqualini@outlook.com>
2023-08-19 16:07:52 +0200

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,27 @@ import {
 	Tooltip,
 	Legend,
 } from 'recharts';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Input } from '@/components/ui/input';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import Footer from './components/footer';
+import { ModeToggle } from './components/mode-toggle';
 
 const App: React.FC = () => {
 	const [teamXG, setTeamXG] = useState([]);
-	const [jsonFile, setJsonFile] = useState('3754125');
+	const [jsonFile, setJsonFile] = useState('110');
 
 	const fetchData = async (file) => {
 		try {
 			const response = await fetch(
-				`https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/${file}.json`
+				`https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/3754${file}.json`
 			);
 			const jsonData = await response.json();
 
@@ -34,9 +46,9 @@ const App: React.FC = () => {
 				return acc;
 			}, {});
 
-			const data = Object.entries(xG).map(([teamName, accumulatedXG]) => ({
+			const data = Object.entries(xG).map(([teamName, AxG]) => ({
 				team: teamName,
-				accumulatedXG,
+				AxG,
 			}));
 
 			setTeamXG(data);
@@ -50,25 +62,51 @@ const App: React.FC = () => {
 	}, [jsonFile]);
 
 	return (
-		<div>
-			<h1 className="text-4xl">Accumulated xG</h1>
-			<div>
-				<label htmlFor="jsonFile">Game number: </label>
-				<input
-					type="text"
-					value={jsonFile}
-					onChange={(e) => setJsonFile(e.target.value)}
-				/>
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+			<div className="flex justify-end m-2">
+				<ModeToggle />
 			</div>
-			<BarChart width={400} height={600} data={teamXG}>
-				<CartesianGrid strokeDasharray="2 2" />
-				<XAxis dataKey="team" />
-				<YAxis />
-				<Tooltip />
-				<Legend />
-				<Bar dataKey="accumulatedXG" fill="cornflowerblue" />
-			</BarChart>
-		</div>
+
+			<div className="flex justify-center m-8">
+				<Card>
+					<CardHeader>
+						<CardTitle>DRAFTPURIST - Expected Goals (xG) Accumulator</CardTitle>
+						<CardDescription>
+							Pair 2 English teams and see how much expected goals they had when
+							faced one another
+						</CardDescription>
+					</CardHeader>
+
+					<CardContent>
+						<p className="font-mono text-sm pb-2">
+							Type a number for a PL match
+						</p>
+						<Input
+							type="text"
+							value={jsonFile}
+							onChange={(e) => setJsonFile(e.target.value)}
+						/>
+
+						<div className="my-8">
+							<BarChart width={600} height={600} data={teamXG}>
+								<CartesianGrid strokeDasharray="6 6" stroke="gray" />
+								<XAxis dataKey="team" />
+								<YAxis />
+								<Tooltip wrapperStyle={{ color: 'cornflowerblue' }} />
+								<Legend />
+								<Bar dataKey="AxG" fill="cornflowerblue" barSize={60} />
+							</BarChart>
+						</div>
+					</CardContent>
+
+					<CardFooter>
+						<img src="/sb.svg" width="100" height="50" />
+					</CardFooter>
+				</Card>
+			</div>
+
+			<Footer />
+		</ThemeProvider>
 	);
 };
 
---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<div className="border-t">
+			<div className="mx-auto py-8">
+				<p className="text-center text-xs">
+					&copy; 2023 DRAFTPURIST. All rights reserved.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
